f386dc223142e65b2b6808e23dfa73b904024b9f
shop <dohongphuc742011@gmail.com>
2026-08-28 22:34:43 +0700

    Update PatchDefinitions.swift

--- 3105-main/ThreeOneOSFive/helpers/PatchDefinitions.swift
+++ 3105-main/ThreeOneOSFive/helpers/PatchDefinitions.swift
@@ -13,29 +13,29 @@ enum PatchDefinitions {
         PatchDefinition(
             id: "aim_body",
             featureName: "Proxy Aim Body",
-            assetNameFFTH: "AimBodyFFTH",
-            assetNameFFMAX: "AimBodyFFMAX",
+            assetNameFFTH: "Aim Body FFTH",
+            assetNameFFMAX: "Aim Body FFMAX",
             targetPath: "Documents/contentcache/Compulsory/ios/gameassetbundles/cache_res.CfnFf59sr1SbsqQ6JqTKsEusjKs~3D"
         ),
         PatchDefinition(
             id: "aim_neck_v1",
             featureName: "Proxy Aim Neck V1",
-            assetNameFFTH: "AimNeckV1FFTH",
-            assetNameFFMAX: "AimNeckV1FFMAX",
+            assetNameFFTH: "Aim Neck V1 FFTH",
+            assetNameFFMAX: "Aim Neck V1 FFMAX",
             targetPath: "Documents/contentcache/Compulsory/ios/gameassetbundles/cache_res.CfnFf59sr1SbsqQ6JqTKsEusjKs~3D"
         ),
         PatchDefinition(
             id: "aim_neck_v2",
             featureName: "Proxy Aim Neck V2",
-            assetNameFFTH: "AimNeckV2FFTH",
-            assetNameFFMAX: "AimNeckV2FFMAX",
+            assetNameFFTH: "Aim Neck V2 FFTH",
+            assetNameFFMAX: "Aim Neck V2 FFMAX",
             targetPath: "Documents/contentcache/Compulsory/ios/gameassetbundles/cache_res.CfnFf59sr1SbsqQ6JqTKsEusjKs~3D"
         ),
         PatchDefinition(
             id: "magic_v4",
             featureName: "Magic V4",
-            assetNameFFTH: "MagicV4FFTH",
-            assetNameFFMAX: "MagicV4FFMAX",
+            assetNameFFTH: "Magic V4 FFTH",
+            assetNameFFMAX: "Magic V4 FFMAX",
             targetPath: "Documents/contentcache/Compulsory/ios/gameassetbundles/cache_res.CfnFf59sr1SbsqQ6JqTKsEusjKs~3D"
         )
     ]
